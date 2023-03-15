c3351c08e8ff077717d615a86cdd1f6a52f29ceb
Felipe <antoniofelipesilva@live.com>
2023-03-15 19:11:43 -0300

    version.1.0.1

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: 'https://swapi.dev/api',
+  baseURL: 'https://jsonplaceholder.typicode.com',
   timeout: 1000,
   headers: {'X-Custom-Header': 'foobar'}
 });
 
 export const api = {
-  getAllmovies: async () => {
-    let response = await http.get('/films/1/');
+  getAllPhotos: async () => {
+    let response = await http.get('/photos');
     return response.data;
   }
 }
